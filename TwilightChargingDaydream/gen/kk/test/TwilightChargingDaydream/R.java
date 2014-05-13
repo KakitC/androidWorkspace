4cc2e7014566f0c55e8a6ef00424fe3248b9105b
KakitC <kitkatcheung@gmail.com>
2014-05-13 11:19:27 -0700

    Started Task Repeater app
    Started new project

--- TwilightChargingDaydream/gen/kk/test/TwilightChargingDaydream/R.java
+++ TwilightChargingDaydream/gen/kk/test/TwilightChargingDaydream/R.java
@@ -10,19 +10,37 @@ package kk.test.TwilightChargingDaydream;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int green=0x7f04000c;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int twilight_sparkle_charging=0x7f020001;
     }
     public static final class id {
-        public static final int gifwebview=0x7f060000;
+        public static final int gifwebview=0x7f070000;
     }
     public static final class layout {
         public static final int daydreamlayout=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int dream_label=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int dream_label=0x7f050001;
     }
     public static final class style {
         /** 
@@ -46,10 +64,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
